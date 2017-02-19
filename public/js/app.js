faeb9072f6dd80cfdc85f5f25c74011f1acd32a8
Blaine Schmeisser <blainesch@gmail.com>
2017-02-19 10:45:30 -0800

    Add html/css keyboard.

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,86 @@ import { Link, Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 require('../css/style.scss')
 
+class Style extends Component {
+  render () {
+    return React.createElement(
+      'style',
+      null,
+      this.props.css
+    )
+  }
+}
+
+class Keyboard extends Component {
+  render () {
+    const css = `
+      .keyboard {
+        width: 600px;
+        margin: 0 auto;
+        text-align: center;
+      }
+      .row span {
+        display: inline-block;
+        border: 1px solid black;
+        padding: 10px 15px;
+        margin: 5px;
+      }
+    `
+
+    return (
+      <div className='keyboard'>
+        <Style css={css} />
+        <div className='row'>
+          <span>q</span>
+          <span>w</span>
+          <span>e</span>
+          <span>r</span>
+          <span>t</span>
+          <span>y</span>
+          <span>u</span>
+          <span>i</span>
+          <span>o</span>
+          <span>p</span>
+        </div>
+        <div className='row'>
+          <span>a</span>
+          <span>s</span>
+          <span>d</span>
+          <span>f</span>
+          <span>g</span>
+          <span>h</span>
+          <span>j</span>
+          <span>k</span>
+          <span>l</span>
+        </div>
+        <div className='row'>
+          <span className='fa fa-arrow-circle-up shift'></span>
+          <span>z</span>
+          <span>x</span>
+          <span>c</span>
+          <span>v</span>
+          <span>b</span>
+          <span>n</span>
+          <span>m</span>
+          <span className='fa fa-arrow-circle-left backspace'></span>
+        </div>
+        <div className='row'>
+          <span className='fa fa-th-list toggle-layout'></span>
+          <span>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </span>
+          <span>.</span>
+          <span>,</span>
+          <span className='fa fa-arrow-circle-down enter'></span>
+        </div>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render () {
     return (
